427a3b7bc34d77a0ca3f64661d247545c7b708cf
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-10-21 19:17:53 +0000

    visibilidade

--- Aulas/Aula03/Aula01.java
+++ Aulas/Aula03/Aula01.java
@@ -1,1 +1,22 @@
 package Aulas.Aula03;
+
+package Aulas.Aula03;
+
+public class Caneta{
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+
+    public escrever(){
+        if (this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar!");
+        }
+        else{
+            System.out.println("Estou Rabiscando!");
+        }
+    }
+}
+
+// Não está correto
