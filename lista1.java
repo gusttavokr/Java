e9e841a07f57ab7d99f0ed89b3838db4fcd7399c
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-10-24 17:18:13 +0000

    aula04

--- lista1.java
+++
@@ -1,9 +0,0 @@
-// import java.util.Scanner;
-
-public class lista1 {
-    public static void main(String[] args) {
-        Circulo c1 = new Circulo();
-        c1.area();
-        c1.circunf();
-    }
-}
